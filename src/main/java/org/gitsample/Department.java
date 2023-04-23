008061432415b8a878fc02de2c293f32a455c784
jkarkar git <jkarkargit@gmail.com>
2023-04-23 11:10:21 +0100

    Added @SuppressWarnings("unused")

--- src/main/java/org/gitsample/Department.java
+++ src/main/java/org/gitsample/Department.java
@@ -2,6 +2,9 @@ package org.gitsample;
 
 public class Department {
 	//department details in my branch
+	@SuppressWarnings("unused")
 	private String name;
+	
+	@SuppressWarnings("unused")
 	private String code;
 }
--- src/main/java/org/gitsample/Employee.java
+++ src/main/java/org/gitsample/Employee.java
@@ -1,7 +1,10 @@
 package org.gitsample;
 //employee details in my branch
 public class Employee {
+	@SuppressWarnings("unused")
 	private String name;
+	
+	@SuppressWarnings("unused")
 	private String address ;
 	
 	
